24f389e1139d11ca20cb5725554b35642f2a4d0f
joelcorey <joelcorey@fastmail.com>
2019-03-25 13:22:20 -0700

    wip

--- src/component/display/display.js
+++ src/component/display/display.js
@@ -3,11 +3,29 @@ import React from 'react';
 export default function Display(props) {
 
   const books = props.books;
-  console.log(books);
+  //console.log(books);
+
+  books.map(book => {
+    if(book.isEbook) {
+
+    } else {
+      return (
+        <div key={book.id}>
+            <h2>{book.title}</h2>
+            <span>{book.author[0]}</span>
+            <div>{book.price || 'Not for sale'}</div>
+            <p>{book.description}</p>
+            <img src={book.imageSmall.smallThumbnail} alt={book.title}/>
+        </div>
+      )
+    }
+
+  })
 
   return (
     <div className='display'>
       {books.map(book => {
+        
         return (
           <div key={book.id}>
             <h2>{book.title}</h2>
--- src/component/searchform/searchform.js
+++ src/component/searchform/searchform.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 export default function SearchForm(props) {
-
+  
   return (
     <div className="search-form">
       <form onSubmit={(event) => {
