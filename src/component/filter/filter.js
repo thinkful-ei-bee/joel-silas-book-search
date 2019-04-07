84ad9432ec500bb3f485155dc030e18b70746a39
joelcorey <joelcorey@fastmail.com>
2019-04-07 10:09:07 -0700

    remove dropdown

--- src/component/filter/filter.js
+++ src/component/filter/filter.js
@@ -6,13 +6,13 @@ export default function Filter(props) {
     
     <div>
       <form>
-        Book type: 
+        {/* Book type: 
         <select>
           <option value="volvo">Volvo</option>
           <option value="saab">Saab</option>
           <option value="mercedes">Mercedes</option>
           <option value="audi">Audi</option>
-        </select> 
+        </select>  */}
         <input id="isEbook" type="checkbox" onChange={props.handleFilterIsEbook} />E-Book
         <input id="isFree" type="checkbox" onChange={props.handleFilterIsBookFree} />Free
         
